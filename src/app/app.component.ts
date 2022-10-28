22ebc448ccedc7999b97abf01b397a48065892fe
sudhandra5048 <112924694+sudhandra5048@users.noreply.github.com>
2022-10-28 13:36:42 +0530

    angular-pipelines

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+ import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { SortPipe } from './sort.pipe';
+@NgModule({
+  declarations: [
+    AppComponent,
+    SortPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SortPipe } from './sort.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SortPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
